f9455be8e7634db7fa0c1389eb244eebce8f54b8
Ravi  Poudel <ravi@supremeitsolutions.com>
2022-05-26 00:54:34 +0545

    feat: custom tabbar with indicator

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom TabBar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TabDemo(),
+    );
+  }
+}
+
+class TabDemo extends StatefulWidget {
+  const TabDemo({Key? key}) : super(key: key);
+
+  @override
+  State<TabDemo> createState() => _TabDemoState();
+}
+
+class _TabDemoState extends State<TabDemo> with TickerProviderStateMixin {
+  late TabController tabController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        bottom: TabBar(
+          controller: tabController,
+          indicatorColor: Colors.transparent,
+          labelPadding: const EdgeInsets.only(bottom: 20),
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          tabs: [
+            _CustomTab(
+              isSelected: _selectedIndex == 0,
+            ),
+            _CustomTab(
+              isSelected: _selectedIndex == 1,
+            ),
+            _CustomTab(
+              isSelected: _selectedIndex == 2,
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: const [
+          Center(child: Text("person 1")),
+          Center(child: Text("person 2")),
+          Center(child: Text("person 3")),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomTab extends StatelessWidget {
+  final bool isSelected;
+
+  const _CustomTab({
+    this.isSelected = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Tab(
+            icon: Icon(
+          Icons.person,
+          size: 30,
+          color: isSelected ? Colors.black : Colors.grey,
+        )),
+        Container(
+          width: 100,
+          height: 10,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: isSelected ? Colors.blue : Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+}
